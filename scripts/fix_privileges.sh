5bebdd5fffcf81020802dc719959dbee83fce45c
Juan Pablo Vila Novas <JPVilaNovas@teco.com.ar>
2021-05-13 21:27:59 -0300

    actualiza el codebuild.spec

--- scripts/fix_privileges.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-chmod +x /home/ec2-user/server/* jar
-chmod +x /home/ec2-user/server/start.sh
-chmod +x /home/ec2-user/server/stop.sh
-chmod +x /home/ec2-user/server/validate.sh
-chmod +x /home/ec2-user/server/server_clear.sh
